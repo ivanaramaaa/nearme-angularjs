20a08c9ee5475a007b661435c33037aa3130be98
Ivana Rodriguez <ivanaramaaa@gmail.com>
2015-03-01 18:22:12 -0500

    Add routes and about page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var app = angular.module("NearMeApp", ['leaflet-directive']);
+var app = angular.module('NearMeApp', ['ngRoute', 'leaflet-directive']);
+  
+  app.config(function ($routeProvider)
+  {
+	$routeProvider
+	  .when('/', {
+	  	templateUrl: 'views/main.html',
+	    controller: 'MainController'
+	  })
+
+	  .when('/about', {
+	  	templateUrl: 'views/about.html',
+	    controller: 'AboutController'
+	  })
+
+	  .otherwise({
+	  	redirectTo: '/'
+	  });
+  });
